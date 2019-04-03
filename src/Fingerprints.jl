94ebf06f09c6b53dcccd763cc56d1aaaac6b038f
Jan Weidner <jw3126@gmail.com>
2019-04-03 12:34:02 +0200

    init

--- src/Fingerprints.jl
+++ src/Fingerprints.jl
@@ -1,5 +1,91 @@
 module Fingerprints
+export fingerprint
 
-greet() = print("Hello World!")
+function namestub(T::Type)::Symbol
+   T.name.name
+end
+
+function namestub(T::UnionAll)::Symbol
+    namestub(T.body)
+end
+
+abstract type Kind end
+struct KindStruct <: Kind end
+struct KindMutable <: Kind end
+struct KindAbstract <: Kind end
+struct KindPrimitive <: Kind end
+struct KindBuiltin <: Kind end
+struct KindUnionAll <: Kind end
+struct KindUnion <: Kind end
+
+@generated function Kind(::Type{T}) where {T}
+    K = if T isa UnionAll
+        KindUnionAll
+    elseif T isa Union
+        KindUnion
+    elseif T.abstract
+        KindAbstract
+    elseif T <: Array
+        KindBuiltin
+    elseif T <: Symbol
+        KindBuiltin
+    elseif T <: String
+        KindBuiltin
+    elseif T <: Tuple
+        KindBuiltin
+    elseif isempty(fieldnames(T)) && (sizeof(T) > 0)
+        KindPrimitive
+    elseif T.mutable
+        KindMutable
+    else
+        KindStruct
+    end
+    :($K())
+end
+
+function fingerprint(::Type{O}, h=zero(UInt64))::UInt64 where {O}
+    hash(namestub(O), h)
+end
+
+function fingerprint(o, h=zero(UInt64))::UInt64
+    _fingerprint(o, h, Kind(typeof(o)))
+end
+
+function _fingerprint(o, h, ::Union{KindPrimitive})
+    hash(o, h)
+end
+
+function _fingerprint(o::Union{Symbol, String}, h, ::KindBuiltin)
+    h = fingerprint(typeof(o), h)
+    hash(o, h)
+end
+
+function _fingerprint(o::Union{Tuple,Array}, h, ::KindBuiltin)
+    h = fingerprint(typeof(o), h)
+    for x in o
+        h = fingerprint(x, h)
+    end
+    h
+end
+
+function _fingerprint_fieldnames_impl(T)
+    ret = quote
+        salt = one(UInt64)
+        h = fingerprint(salt, h)
+        h = fingerprint($T, h)
+    end
+    for s in fieldnames(T)
+        ex = :(
+            h = fingerprint($(QuoteNode(s)), h);
+            h = fingerprint(o.$s, h))
+        push!(ret.args, ex)
+    end
+    push!(ret.args, :h)
+    ret
+end
+
+@generated function _fingerprint(o, h, ::Union{KindStruct, KindMutable})
+    _fingerprint_fieldnames_impl(o)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,94 @@
 using Fingerprints
+const FP = Fingerprints
 using Test
 
-@testset "Fingerprints.jl" begin
-    # Write your own tests here.
+abstract type AAbstractType end
+struct AStruct end
+mutable struct AMutableStruct end
+primitive type APrimitiveType 64 end
+
+@testset "namestub" begin
+    @test FP.namestub(AAbstractType)   === :AAbstractType
+    @test FP.namestub(AStruct)         === :AStruct
+    @test FP.namestub(AMutableStruct)  === :AMutableStruct
+    @test FP.namestub(APrimitiveType)  === :APrimitiveType
+    @test FP.namestub(Vector{Float64}) === :Array
+    @test FP.namestub(Vector) === :Array
+    @test FP.namestub(Array) === :Array
 end
+
+@testset "Kind" begin
+    @test FP.Kind(AMutableStruct)      === FP.KindMutable()
+    @test FP.Kind(AStruct)             === FP.KindStruct()
+    @test FP.Kind(AAbstractType)       === FP.KindAbstract()
+    @test FP.Kind(APrimitiveType)      === FP.KindPrimitive()
+    @test FP.Kind(Vector)              === FP.KindUnionAll()
+    @test FP.Kind(Union{Vector, Int})  === FP.KindUnion()
+    @test FP.Kind(AbstractArray)       === FP.KindUnionAll()
+    @test FP.Kind(AbstractVector{Int}) === FP.KindAbstract()
+    @test FP.Kind(Vector{Int})         === FP.KindBuiltin()
+    @test FP.Kind(Matrix{Int})         === FP.KindBuiltin()
+    @test FP.Kind(Symbol)              === FP.KindBuiltin()
+    @test FP.Kind(String)              === FP.KindBuiltin()
+end
+
+module A
+    struct S
+        x
+        y
+    end
+end
+
+module B
+    struct S
+        x
+        y
+    end
+    struct T
+        x
+        y
+    end
+end
+
+module C
+    struct S
+        x
+        z
+    end
+end
+
+@testset "Module name invariance" begin
+    @test fingerprint(A.S(1,2)) === fingerprint(B.S(1,2))
+    @test fingerprint(A.S(1,2)) !== fingerprint(B.T(1,2))
+    @test fingerprint(A.S(1,2)) !== fingerprint(C.S(1,2))
+end
+
+
+struct E1 end
+struct E2 end
+
+@testset "empty structs" begin
+    @test fingerprint(E1())  !== fingerprint(E2())
+    @test fingerprint(E1())  !== fingerprint(E1)
+    @test fingerprint(E1())  === fingerprint(E1())
+    @test fingerprint("hi")  !== fingerprint(:hi)
+end
+
+@testset "mutable structs" begin
+    mutable struct M1
+        x
+    end
+    @test fingerprint(M1(1)) === fingerprint(M1(1))
+    @test fingerprint(M1(1)) !== fingerprint(M1(2))
+end
+
+struct Container{T}
+    inner::T
+end
+@testset "persistence" begin
+    @test fingerprint(Container(Container("1"))) === 0x182a028725dce688
+end
+
+@test fingerprint([1,2]) != fingerprint((1,2))
+@test fingerprint((1,2)) != fingerprint((a=1,b=2))
+@test fingerprint((a=2,b=1)) != fingerprint((a=1,b=2))
